f8238bba9117a37383e7eeefc6606b5a95c2c020
李赓 <ligeng@1dabang.cn>
2017-09-14 14:26:32 +0800

    update

--- mongo-shard-config/start.sh
+++ mongo-shard-config/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for (( i=1; i<=3; i=i+1 ))
 do
-	nohup mongod --replSet shard$i --shardsvr --bind_ip 192.168.199.137  --port 300$i --dbpath /Users/ligeng/Documents/mongo-shard-data/300$i >300$i.log 2>&1 &
-	nohup mongod --replSet shard$i --shardsvr --bind_ip 192.168.199.137  --port 400$i --dbpath /Users/ligeng/Documents/mongo-shard-data/400$i >400$i.log 2>&1 &
-	nohup mongod --replSet shard$i --shardsvr --bind_ip 192.168.199.137  --port 500$i --dbpath /Users/ligeng/Documents/mongo-shard-data/500$i >500$i.log 2>&1 &
+	nohup mongod --replSet shard$i --shardsvr --bind_ip 0.0.0.0  --port 300$i --dbpath /Users/ligeng/Documents/mongo-shard-data/300$i >300$i.log 2>&1 &
+	nohup mongod --replSet shard$i --shardsvr --bind_ip 0.0.0.0  --port 400$i --dbpath /Users/ligeng/Documents/mongo-shard-data/400$i >400$i.log 2>&1 &
+	nohup mongod --replSet shard$i --shardsvr --bind_ip 0.0.0.0  --port 500$i --dbpath /Users/ligeng/Documents/mongo-shard-data/500$i >500$i.log 2>&1 &
 #!	echo $i
 #!	for (( j=1; j<=3; j=j+1 ))
 #!	do
